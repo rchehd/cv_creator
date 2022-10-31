fdff83f642012b2c3c34708ed8ca6e6e1f675f29
Roman <roman.chekhaniuk@smile-ukraine.com>
2022-10-31 15:26:44 +0200

    Add footer and header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import Header from './components/Header'
+import Footer from './components/Footer'
 </script>
 
 <template>
   <div class="container">
-  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="32" />
-    </a>
-    <ul class="nav nav-pills">
-      <li class="nav-item"><RouterLink to="/"><a href="#" class="nav-link" aria-current="page">Home</a></RouterLink></li>
-      <li class="nav-item"><RouterLink to="/about"><a href="#" class="nav-link">About</a></RouterLink></li>
-    </ul>
-  </header>
+    <Header></Header>
+    <RouterView />
+    <Footer></Footer>
   </div>
-  <RouterView />
+
 </template>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="32" />
+    </a>
+    <ul class="nav nav-pills">
+      <li class="nav-item"><RouterLink to="/"><a href="#" class="nav-link" aria-current="page">Home</a></RouterLink></li>
+      <li class="nav-item"><RouterLink to="/about"><a href="#" class="nav-link">About</a></RouterLink></li>
+    </ul>
+  </header>
+</template>
