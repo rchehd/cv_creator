b7befc32e5ccb899379f59ca1be30065f9cfd613
Roman <roman.chekhaniuk@smile-ukraine.com>
2022-10-31 14:27:57 +0200

    Change styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,10 +9,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
       <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="32" />
     </a>
-
     <ul class="nav nav-pills">
-      <li class="nav-item"><RouterLink to="/">Home</RouterLink></li>
-      <li class="nav-item"><RouterLink to="/about">About</RouterLink></li>
+      <li class="nav-item"><RouterLink to="/"><a href="#" class="nav-link" aria-current="page">Home</a></RouterLink></li>
+      <li class="nav-item"><RouterLink to="/about"><a href="#" class="nav-link">About</a></RouterLink></li>
     </ul>
   </header>
   </div>
